4e951a44f052d312553d61d32af87bd03a216a42
PigHousePig <alex.iong200410@gmail.com>
2025-03-11 15:16:51 +0800

    Implement functions Complete free queue, insert/remove head, tail, delete middle
    Change-Id: Ifd5b7f9d2ab4512bf386fc1a1d0c3a7ff897aaae

--- queue.c
+++ queue.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "harness.h"
+#include "list.h"
 
 #include "queue.h"
 
@@ -15,43 +17,83 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    element_t *entry, *safe;
+    list_for_each_entry_safe(entry, safe, head, list) {
+        q_release_element(entry);
+    }
+    test_free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    element_t *node = malloc(sizeof(element_t));
+    if (!head || !node)
+        return false;
+    node->value = strdup(s);
+    list_add(&node->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    element_t *node = malloc(sizeof(element_t));
+    if (!head || !node)
+        return false;
+    node->value = strdup(s);
+    list_add_tail(&node->list, head);
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+    element_t *node = list_first_entry(head, element_t, list);
+    list_del(&node->list);
+    return node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+    element_t *node = list_last_entry(head, element_t, list);
+    list_del(&node->list);
+    return node;
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+    struct list_head *node;
+    int num = 0;
+    list_for_each(node, head) {
+        num++;
+    }
+    return num;
 }
 
 /* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
-    return true;
+    if (!head || list_entry(head, element_t, list))
+        return false;
+    struct list_head *slow, *fast;
+    for (slow = fast = head->next; fast != head && fast->next != head;
+         slow = slow->next, fast = fast->next->next)
+        /* iterate */;
+    list_del(slow);
+    free(slow);
+    return false;
 }
 
 /* Delete all nodes that have duplicate string */
